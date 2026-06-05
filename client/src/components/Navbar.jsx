e88752386d564f4f458012316b22584d48c7c79b
Saatvik Chauhan <saatvikchauhan03@gmail.com>
2026-06-05 13:03:28 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FiMenu, FiX } from 'react-icons/fi';
-import './Navbar.css';
 
 const navLinks = [
   { label: 'Home', path: '/' },
